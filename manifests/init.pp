8faf7dca95276e020a7c986a17be3c320f22db19
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-09-25 14:07:00 +0200

    fixing run-once errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,8 @@
 # Copyright 2014
 #
 class security (
-
   $securitypackage            = 'none',
   $security_status            = 'latest',
-
   ) {
 
   if $securitypackage == 'none' {
@@ -19,59 +17,44 @@ class security (
       path                    => '/var/lock/puppet-once',
     }
   }
-
   else {
-
   Exec {
     refreshonly               => true,
   }
-
   case $operatingsystem {
-
     'Ubuntu': {
-
       exec { 'run-once-commands':
         command               => 'touch /var/lock/puppet-once',
         path                  => '::path',
         creates               => '/var/lock/puppet-once',
         notify                => Exec['apt-get update'],
       }
-
       exec { 'apt-get update':
         command               => '/usr/bin/apt-get update',
       }
-
-      package { '$securitypackage':
-        ensure                => '$security_status',
+      package { $securitypackage:
+        ensure                => $security_status,
         require               => Exec['apt-get update'],
       }
-
     }
-
     'CentOS': {
-
       exec { 'run-once-commands':
         command               => 'touch /var/lock/puppet-once',
         path                  => '::path',
         creates               => '/var/lock/puppet-once',
         notify                => Exec['yum update'],
       }
-
       exec { 'yum update':
         command               => '/usr/bin/yum update',
       }
-
-      package { '$securitypackage':
-        ensure                => '$security_status',
+      package { $securitypackage:
+        ensure                => $security_status,
         require               => Exec['yum update'],
       }
-
     }
-
   'default': {
     notify { "Security fixes on '$operatingsystem' - '$operatingsystemrelease' are not supported": }
   }
-
 }
 }
 }
