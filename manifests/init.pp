3af1224d8b535ecbff9fb6e89906251f6ccc962f
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-09-25 13:57:47 +0200

    fixing run-once errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,58 +7,71 @@
 # Copyright 2014
 #
 class security (
-  $securitypackage = 'none',
-  $security_status = 'latest',
+
+  $securitypackage            = 'none',
+  $security_status            = 'latest',
+
   ) {
 
   if $securitypackage == 'none' {
     file {'/var/lock/puppet-once':
-      ensure  => absent,
-      path    => '/var/lock/puppet-once',
+      ensure                  => absent,
+      path                    => '/var/lock/puppet-once',
     }
   }
 
   else {
 
-# Default setting for exec command
   Exec {
-    refreshonly => true,
+    refreshonly               => true,
   }
 
   case $operatingsystem {
+
     'Ubuntu': {
+
       exec { 'run-once-commands':
-        command => 'touch /var/lock/puppet-once',
-        path    => '::path',
-        creates => '/var/lock/puppet-once',
-        notify  => [Exec['apt-get update']],
+        command               => 'touch /var/lock/puppet-once',
+        path                  => '::path',
+        creates               => '/var/lock/puppet-once',
+        notify                => Exec['apt-get update'];
       }
+
       exec { 'apt-get update':
-        command                         => '/usr/bin/apt-get update',
+        command               => '/usr/bin/apt-get update',
       }
+
       package { '$securitypackage':
-        ensure                          => '$security_status',
-        require                         => Exec['apt-get update'],
+        ensure                => '$security_status',
+        require               => Exec['apt-get update'],
       }
+
     }
+
     'CentOS': {
+
       exec { 'run-once-commands':
-        command => 'touch /var/lock/puppet-once',
-        path    => '::path',
-        creates => '/var/lock/puppet-once',
-        notify  => Exec['yum update']],
+        command               => 'touch /var/lock/puppet-once',
+        path                  => '::path',
+        creates               => '/var/lock/puppet-once',
+        notify                => Exec['yum update'];
       }
+
       exec { 'yum update':
-        command                       => '/usr/bin/yum update',
+        command               => '/usr/bin/yum update',
       }
+
       package { '$securitypackage':
-        ensure                          => '$security_status',
-        require                         => Exec['yum update'],
+        ensure                => '$security_status',
+        require               => Exec['yum update'],
       }
+
     }
+    
   'default': {
     notify { "Security fixes on '$operatingsystem' - '$operatingsystemrelease' are not supported": }
   }
+
 }
 }
 }
